5626767e2c606b00ffa78bf119139d9b5023335e
Philip Hartout <philip.hartout@protonmail.com>
2020-03-11 18:30:55 +0100

    :boom: add formatted script w/ sequential approach

--- projects/Project_1/Task_1b/task_1b.py
+++ projects/Project_1/Task_1b/task_1b.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This script is for project1, task b, which endeavours to perform linear regression with
-feature transformation
+This script is for project1, task b, which endeavours to perform ridge
+regression using feature transformation
 
 example usage from CLI:
- $ python3 task_1b.py --train ~/path/to/train/dir/ --weights ~/path/to/weights/file
+ $ python3 task_1b.py --train ~/path/to/train/dir/
+                      --weights ~/path/to/weights/file
 
 For help, run:
  $ python3 task_1b.py -h
 
-To do:
-- Improve score to meet hard baseline from project
+Following Google style guide: http://google.github.io/styleguide/pyguide.html
+
 """
 
+__author__ = "Josephine Yates; Philip Hartout; Flavio Rump"
+__email__ = (
+    "jyates@student.ethz.ch; phartout@student.ethz.ch; flrump@student.ethz.ch"
+)
+
+
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import RidgeCV,LassoCV,Ridge,Lasso
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.linear_model import RidgeCV, LassoCV, Ridge, Lasso
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
@@ -24,184 +33,123 @@ import pandas as pd
 import numpy as np
 import argparse
 
-__author__ = "Josephine Yates; Philip Hartout; Flavio Rump"
-__email__ = "jyates@student.ethz.ch; phartout@student.ethz.ch; flrump@student.ethz.ch"
 
-
-def rmse(y_true, y_pred):
-    """This function computes the RMSE of a vector of predicted and true labels
-
-    Args:
-        y_true (numpy.ndarray): Vector of true labels.
-        y_pred (numpy.ndarray): Vector of predicted labels.
-
-    Returns:
-        float: the computed root mean square error
-    """
-    return mean_squared_error(y_true, y_pred) ** 0.5
-
-
-def feature_transformation(x):
-    """This function calculates new df with transformed features - linear,
-    quadratic, exponential, cosine and constant - starting with
-    X = [x1,x2,x3,x4,x5]
-
-    Args:
-        x (pandas.core.frame.DataFrame): Matrix containing the training data.
-
-    Returns:
-        pandas.core.frame.DataFrame: the transformed features
-    """
-    # apply function to df
-    qx = x.apply(np.square)
-    # rename columns
-    qx.columns = ["phi6", "phi7", "phi8", "phi9", "phi10"]
-
-    ex = x.apply(np.exp)
-    ex.columns = ["phi11", "phi12", "phi13", "phi14", "phi15"]
-
-    cx = x.apply(np.cos)
-    cx.columns = ["phi16", "phi17", "phi18", "phi19", "phi20"]
-
-    cst = pd.DataFrame(np.ones((x.shape[0],)), columns=["phi21"])
-
-    x.columns = ["phi1", "phi2", "phi3", "phi4", "phi5"]
-
-    return pd.concat([x, qx, ex, cx, cst], axis=1)
-
-
-def evaluate_model(x_train, y_train, alpha, model_train):
-    """This function computes the average RMSE based on k-fold cross-validation
-
-    Args:
-        x_train (numpy.ndarray): Matrix of training samples.
-        y_train (numpy.ndarray): Vector of training labels.
-
-    Returns:
-        float: the average root mean square error
-    """
-    n_splits = 10
-    kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
-    rmse_avg=0
-    for train_index, test_index in kf.split(x_train):
-        # separate for cross validation
-        x_cv, x_test_cv = x_train[train_index], x_train[test_index]
-        y_cv, y_test_cv = y_train[train_index], y_train[test_index]
-        # fit the model
-        if model_train=="ridge":
-            model = Ridge(alpha=alpha).fit(x_cv,y_cv)
-        elif model_train=="lasso":
-            model = Lasso(alpha=alpha).fit(x_cv,y_cv)
-        # predict
-        y_pred = model.predict(x_test_cv)
-        # calculate average RMSE
-        rmse_avg += rmse(y_test_cv, y_pred)
-    return rmse_avg/n_splits
-
-def plot_error_model(alphas,mean_errors):
-    """This function plots the mean error of CV vs the regularization parameter in ridge regression
+def plot_error_model(alphas, mean_errors):
+    """This function plots the mean error of CV vs the regularization parameter
+    of the model being evaludated.
 
     Args:
         alphas (numpy.ndarray): regularization parameters tried out
-        mean_errors (numpy.ndarray): result of average error over cross validation for different alphas
+        mean_errors (numpy.ndarray): result of average error over cross
+            validation for different alphas
 
     Returns:
         None
     """
-    plt.plot(alphas,mean_errors)
+    plt.plot(alphas, mean_errors)
     plt.title("Mean cv error vs regularization parameter")
     plt.xlabel("Alpha, regularization parameter")
     plt.ylabel("Mean error")
     plt.show()
 
 
-def main(method="ridge"):
+def linear(x):
+    """Returns the same array as passed
+
+    Args:
+        x (numpy.ndarray): vector or array of numbers to be passed
+
+    Returns:
+        The same array
+    """
+    return x
+
+
+def constant(x):
+    """Returns a vector containing ones the dimensions of the input array
+
+    Args:
+        x (numpy.ndarray): vector or array containing data
+
+    Returns:
+        An array of ones of the same length as the input
+    """
+    return np.ones((x.shape[0],)).reshape(-1, 1)
+
+
+def main():
     # Load training set
     df_train = pd.read_csv(FLAGS.train, header=0, index_col=0)
 
-   x # Process for modelling
-    x_train, y_train = df_train.drop(['y'], axis=1), df_train['y'].values
+    # Process for modelling
+    x_train, y_train = df_train.drop(["y"], axis=1), df_train["y"].values
 
-    # Create the dataset with transformed features
-    x_train = feature_transformation(x_train).values
-
-    # Scale the data
+    # Declare scaler
     scaler = StandardScaler(with_mean=True, with_std=True)
-    x_train = pd.DataFrame(scaler.fit_transform(x_train)).values
 
-    # Train the model and get models
-    if method=="LR":
-        print("Starting regression with classical linear regression...")
-        model = LinearRegression().fit(x_train, y_train)
-        weights = model.coef_
-        reg_score = model.score(x_train, y_train)
-        print(f"The reg score is {reg_score}")
-        score = evaluate_regression(x_train, y_train)
-        print(f"Average RMSE on 10-fold cv is {score}")
+    # Declare transformations to be done
+    transformations = [linear, np.square, np.exp, np.cos, constant]
+    weights_list = []
 
-    # score to beat: 4.89
-    elif method=="ridge":
-        print("Starting regression with ridge regression...")
-        alphas = np.arange(10000,10000,1)
-        rmse=[]
-        for alpha in alphas:
-            rmse.append(evaluate_model(x_train,y_train,alpha,"ridge"))
-        #plot_error_model(alphas,rmse)
-        alpha = alphas[np.argmin(rmse)]
-        print("Alpha is {}".format(alpha))
-        model = Ridge(alpha=alpha).fit(x_train,y_train)
-        weights = model.coef_
-        score = np.min(rmse)
-        # plot_error_model(alphas,mean_errors)
-        print(f"Average RMSE on 10-fold cv is {score}")
-        #print("The estimated alpha is {}".format(alpha))
+    # loop through data transformations
+    for transformation in transformations:
+        # Transform the data
+        transformer = FunctionTransformer(transformation)
+        x_train_trans = transformer.transform(x_train)
 
+        # Scale the data
+        x_train_trans_scaled = scaler.fit_transform(x_train_trans)
 
-    elif method=="ridgeCV":
-        print("Starting regression with ridge LOOCV...")
-        alphas = np.arange(1.0,15,0.1)
-        model = RidgeCV(alphas=alphas,fit_intercept=True,normalize=True,scoring=None,
-                            cv=None,gcv_mode=None,store_cv_values=True).fit(x_train, y_train)
-        errors = model.cv_values_
-        print(errors.shape)
-        mean_errors = [np.mean(errors[:,i]) for i in range(errors.shape[1])]
-        alpha = model.alpha_
-        weights = model.coef_
-        score = evaluate_regression(x_train, y_train)
-        # plot_error_model(alphas,mean_errors)
-        print(f"Average RMSE on 10-fold cv is {score}")
-        #print("The estimated alpha is {}".format(alpha))
+        # Specify the alpha range to use
+        alpha_range = np.arange(0.1, 10, 0.1)
 
-    elif method=="lasso":
-        print("Starting regression with lasso regression...")
-        alphas = np.arange(0.01,5,0.01)
-        rmse=[]
-        for alpha in alphas:
-            rmse.append(evaluate_model(x_train,y_train,alpha,"lasso"))
-        plot_error_model(alphas,rmse)
-        alpha = alphas[np.argmin(rmse)]
-        print("Alpha is {}".format(alpha))
-        model = Lasso(alpha=alpha).fit(x_train,y_train)
-        weights = model.coef_
+        # Fit the regression with RidgeCV
+        reg = RidgeCV(
+            fit_intercept=False, alphas=alpha_range, store_cv_values=True
+        ).fit(x_train_trans_scaled, y_train)
+
+        # Append coefs to the list of weights
+        weights_list.append(reg.coef_)
+
+        # Compute errors for plotting and analysing alpha values
+        errors = reg.cv_values_
+        mean_errors = [np.mean(errors[:, i]) for i in range(errors.shape[1])]
+        plot_error_model(alpha_range, mean_errors)
+        print(f"Selected value for alpha is: {reg.alpha_}")
+
+    # flatten list coefficients
+    weights_list_flat = [item for sublist in weights_list for item in sublist]
 
     # export to .csv
-    weight_df = pd.DataFrame(weights)
-    weight_df.to_csv(FLAGS.weights, sep=" ", index=False, header=False, float_format='%.2f')
+    weight_df = pd.DataFrame(weights_list_flat)
+    weight_df.to_csv(
+        FLAGS.weights, sep=" ", index=False, header=False, float_format="%.5f"
+    )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='CLI args for folder and file \
-    directories')
-    parser.add_argument("--train", "-tr", type=str, required=True,
-                        help="path to the CSV file containing the training \
-                        data")
-    parser.add_argument("--weights", "-w", type=str, required=True,
-                        help="path where the CSV file where weights should be \
-                        written")
-    parser.add_argument("--method", "-m", type=str, required=True,
-                        help="method used for optimization, can be LR \
-                        (classical Linear Regression), ridge (ridge regr with \
-                        CV with RMSE), ridgeCV (ridge regr with LOOCV with LS) \
-                        or lasso (lasso regr with CV with RMSE)")
+
+    parser = argparse.ArgumentParser(
+        description="CLI args for folder and file \
+    directories"
+    )
+
+    parser.add_argument(
+        "--train",
+        "-tr",
+        type=str,
+        required=True,
+        help="path to the CSV file containing the training \
+                        data",
+    )
+    parser.add_argument(
+        "--weights",
+        "-w",
+        type=str,
+        required=True,
+        help="path where the CSV file where weights should be \
+                        written",
+    )
+
     FLAGS = parser.parse_args()
-    main(FLAGS.method)
+    main()
