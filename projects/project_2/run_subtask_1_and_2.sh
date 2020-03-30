c8dcd1c0efe07006c18a2030cd0d26bae16864ed
Philip Hartout <philip.hartout@protonmail.com>
2020-03-30 17:06:16 +0200

    :shell: add temporary shell script to run script

---
+++ projects/project_2/run_subtask_1_and_2.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate iml
+
+python /home/philiphartout/Documents/GitHub/Introduction_to_Machine_Learning/projects/project_2/subtask1and2.py
+exit
