4c15e1e434731fc6e1afa313a7740b16b3791631
Philip Hartout <philip.hartout@protonmail.com>
2020-04-01 17:54:11 +0200

    :crystal_ball: upload working SVR model for vital signs

--- projects/project_2/subtask3.py
+++ projects/project_2/subtask3.py
@@ -34,7 +34,7 @@ import pandas as pd
 import numpy as np
 from imblearn.over_sampling import ADASYN, SMOTE
 from imblearn.under_sampling import ClusterCentroids, RandomUnderSampler
-from sklearn.svm import LinearSVC, SVC
+from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
@@ -78,28 +78,29 @@ TYPICAL_VALUES = {
     "pH": 7.367231494050988,
 }
 
-INDENTIFIERS = ["pid", "Time"]
+IDENTIFIERS = ["pid", "Time"]
 MEDICAL_TESTS = [
-        "LABEL_BaseExcess",
-        "LABEL_Fibrinogen",
-        "LABEL_AST",
-        "LABEL_Alkalinephos",
-        "LABEL_Bilirubin_total",
-        "LABEL_Lactate",
-        "LABEL_TroponinI",
-        "LABEL_SaO2",
-        "LABEL_Bilirubin_direct",
-        "LABEL_EtCO2",
-    ]
-vital_signs = ["LABEL_RRate", "LABEL_ABPm", "LABEL_SpO2", "LABEL_Heartrate"]
-sepsis = ["LABEL_Sepsis"]
+    "LABEL_BaseExcess",
+    "LABEL_Fibrinogen",
+    "LABEL_AST",
+    "LABEL_Alkalinephos",
+    "LABEL_Bilirubin_total",
+    "LABEL_Lactate",
+    "LABEL_TroponinI",
+    "LABEL_SaO2",
+    "LABEL_Bilirubin_direct",
+    "LABEL_EtCO2",
+]
+VITAL_SIGNS = ["LABEL_RRate", "LABEL_ABPm", "LABEL_SpO2", "LABEL_Heartrate"]
+SEPSIS = ["LABEL_Sepsis"]
 
 
 def load_data():
     """Loads data to three different dataframes.
 
     Returns:
-        df_train, df_train_label, df_test (pandas.core.frame.DataFrame): three dataframes containing the
+        df_train, df_train_label, df_test (pandas.core.frame.DataFrame): three dataframes
+        containing the
         training features, training labels and testing features respectively.
 
     """
@@ -141,67 +142,13 @@ def fill_na_with_average_patient_column(df, logger):
     return df
 
 
-def fill_na_with_average_column(df):
-    """Quick version of fill_na_with_average_patient_column - does not support patient average
-    and results in loss of information.
-
-    Note:
-        Inserted dict with typical values as global var because running the script on parts of the data
-    leads to errors associated with NaNs because there is not a single sample.
-
-    Args:
-        df (pandas.core.DataFrame): data to be transformed
-
-    Returns:
-        df (pandas.core.frame.DataFrame): dataframe containing the transformed data
-    """
-
-    df = df.fillna(df.mean(numeric_only=True))
-    if df.isnull().values.any():
-        columns_with_na = df.columns[df.isna().any()].tolist()
-        for column in columns_with_na:
-            df[column] = TYPICAL_VALUES[column]
-    return df
-
-
-def oversampling_strategies(X_train, y_train, strategy):
-    """Function that uses a strategy to balance the dataset.
-
-    Args:
-        X_train (numpy.ndarray): array containing the features that need to be balanced
-        y_train (numpy.ndarray): array containing the data labels
-        strategy (int): sampling strategy to be adopted
-
-    Returns:
-        X_train_resampled, y_train_resampled (numpy.ndarray): resampled features and labels according
-        to the chosen strategy
-    """
-
-    # Oversampling methods
-    if strategy == "adasyn":
-        sampling_method = ADASYN()
-    if strategy == "smote":
-        sampling_method = SMOTE()
-
-    # Undersampling methods
-    if strategy == "clustercentroids":
-        sampling_method = ClusterCentroids(random_state=42)
-    if strategy == "random":
-        sampling_method = RandomUnderSampler(random_state=0, replacement=True)
-
-    X_train_resampled, y_train_resampled = sampling_method.fit_sample(X_train, y_train)
-
-    return X_train_resampled, y_train_resampled
-
-
 def data_preprocessing(df_train, df_train_label, df_test, logger):
-
     logger.info("Preprocess training set")
     # Would be useful to distribute/multithread this part
     df_train_preprocessed = fill_na_with_average_patient_column(df_train, logger)
 
     logger.info("Perform projection to select only vital signs labels")
-    df_train_label = df_train_label[vital_signs]
+    df_train_label = df_train_label[["pid"] + VITAL_SIGNS]
 
     # Merging pids to make sure they map correctly.
     df_train_preprocessed_merged = pd.merge(
@@ -209,17 +156,16 @@ def data_preprocessing(df_train, df_train_label, df_test, logger):
     )
     # Cast to arrays
     X_train = df_train_preprocessed_merged.drop(
-        columns=INDENTIFIERS  + vital_signs
+        columns=IDENTIFIERS + VITAL_SIGNS
     ).values
     # Create list with different label for each medical test
     logger.info("Creating a list of labels for each medical test")
     y_train_vital_signs = []
-    for test in vital_signs:
-        y_train_vital_signs.append(df_train_preprocessed_merged[test].values)
+    for sign in VITAL_SIGNS:
+        y_train_vital_signs.append(df_train_preprocessed_merged[sign].values)
 
     logger.info("Preprocess test set")
-    df_test_preprocessed = fill_na_with_average_patient_column(df_train, logger)
-
+    df_test_preprocessed = fill_na_with_average_patient_column(df_test, logger)
     # Scale data to avoid convergence warning
     logger.info(f"Scaling data using {FLAGS.scaler}.")
 
@@ -229,8 +175,56 @@ def data_preprocessing(df_train, df_train_label, df_test, logger):
         scaler = MinMaxScaler()
 
     X_train = scaler.fit_transform(X_train)
-    
-    return X_train_resampled_sepsis, y_train_resampled_sepsis
+
+    return X_train, y_train_vital_signs, df_test_preprocessed
+
+
+def get_vital_signs_models(X_train, y_train_vital_signs, param_grid):
+    svr_models = []
+    scores = []
+    for i, test in enumerate(VITAL_SIGNS):
+        logger.info(f"Starting iteration for test {test}")
+        svr = SVR()
+        cores = multiprocessing.cpu_count() - 2
+        tuned_svr = GridSearchCV(
+            estimator=svr,
+            param_grid=param_grid,
+            n_jobs=cores,
+            scoring="r2",
+            cv=FLAGS.k_fold,
+            verbose=0,
+        )
+        tuned_svr.fit(X_train, y_train_vital_signs[i])
+        svr_models.append(tuned_svr.best_estimator_)
+        scores.append(tuned_svr.best_score_)
+
+    return svr_models, scores
+
+
+def get_vital_signs_predictions(X_test, test_pids, models):
+    """Function to obtain predictions for every model, as a confidence level : the closer to 1
+    (resp 0), the more confidently) the sample belongs to class 1 (resp 0).
+
+    Args:
+        X_test (np.ndarray): array of preprocessed test values
+        test_pids (np.ndarray): array of patient ids in test set
+        svm_models (list): list of models for each of the medical tests
+
+    Returns:
+        df_pred (pandas.core.DataFrame): contains the predictions made by each of the models for
+        their respective tests, containing for each patient id the predicted label as a confidence
+        level.
+    """
+    df_pred = pd.DataFrame()
+
+    for i, test in enumerate(VITAL_SIGNS):
+        # Compute prediction
+        y_pred = models[i-1].predict(X_test)
+
+        y_mean = [np.mean(y_pred[i: i + 12]) for i in range(len(test_pids))]
+        df = pd.DataFrame({test: y_mean}, index=test_pids)
+        df_pred = pd.concat([df_pred, df], axis=1)
+    return df_pred
 
 
 def main(logger):
@@ -247,36 +241,37 @@ def main(logger):
     df_train, df_train_label, df_test = load_data()
     logger.info("Finished Loading data")
 
-    data_preprocessing(df_train, df_train_label, df_test, logger)
+    X_train, y_train_vital_signs, df_test_preprocessed = data_preprocessing(df_train,
+                                                                            df_train_label,
+                                                                            df_test, logger)
 
     logger.info("Beginning modelling process.")
 
     # train model here
-
-    X_test = df_test_preprocessed.drop(columns=INDENTIFIERS).values
+    param_grid = {
+        "kernel": ['linear', 'poly', 'rbf', 'sigmoid'],
+        "degree": np.arange(1, 4, 1),
+        "gamma": np.linspace(0.1, 10, num=3),
+        "coef0": [0],
+        "tol": [0.001],
+        "C": np.linspace(0.1, 10, num=3),
+        "epsilon": [0.1],
+        "shrinking": [True],
+        "cache_size": [1000],
+        "verbose": [False],
+        "max_iter": [1000]
+    }
+    vital_signs_models, vital_signs_models_scores = get_vital_signs_models(X_train, y_train_vital_signs, param_grid)
 
     # get the unique test ids of patients
-    test_pids = np.unique(df_test_preprocessed[["pid"]].values)
+    test_pids = np.unique(df_test_preprocessed[IDENTIFIERS].values)
     logger.info("Fetch predictions.")
-    medical_test_predictions = get_medical_test_predictions(
-        X_test, test_pids, best_model_ MEDICAL_TESTS, MEDICAL_TESTS
-    )
-    sepsis_predictions = get_sepsis_predictions(
-        X_test, test_pids, best_model_sepsis, sepsis
-    )
-    medical_test_predictions.index.names = ["pid"]
-    sepsis_predictions.index.names = ["pid"]
-    predictions = pd.merge(
-        medical_test_predictions,
-        sepsis_predictions,
-        how="left",
-        left_on="pid",
-        right_on="pid",
-    )
+    X_test = df_test_preprocessed.drop(columns=IDENTIFIERS).values
+    vital_signs_predictions = get_vital_signs_predictions(X_test, test_pids, vital_signs_models)
 
     logger.info("Export predictions DataFrame to a zip file")
     # Export pandas dataframe to zip archive.
-    predictions.to_csv(
+    vital_signs_predictions.to_csv(
         FLAGS.predictions, index=False, float_format="%.3f", compression="zip"
     )
 
@@ -293,7 +288,7 @@ if __name__ == "__main__":
         type=str,
         required=True,
         help="path to the CSV file containing the training \
-                        features",
+      features",
     )
 
     parser.add_argument(
@@ -302,7 +297,7 @@ if __name__ == "__main__":
         type=str,
         required=True,
         help="path to the CSV file containing the testing \
-                            features",
+ features",
     )
 
     parser.add_argument(
@@ -311,7 +306,7 @@ if __name__ == "__main__":
         type=str,
         required=True,
         help="path to the CSV file containing the training \
-                                labels",
+     labels",
     )
 
     parser.add_argument(
@@ -320,7 +315,7 @@ if __name__ == "__main__":
         type=str,
         required=True,
         help="path to the zip file containing the \
-                                predictions",
+     predictions",
     )
 
     parser.add_argument(
@@ -329,16 +324,6 @@ if __name__ == "__main__":
         type=int,
         required=False,
         help="Number of patients to consider in run. If not specified, then consider all patients",
-    )
-
-    parser.add_argument(
-        "--sampling_strategy",
-        "-samp",
-        type=str,
-        required=True,
-        help="Sampling strategy to adopt to overcome the imbalanced dataset problem"
-        "any of adasyn, smote, clustercentroids or random.",
-        choices=["adasyn", "smote", "clustercentroids", "random"],
     )
 
     parser.add_argument(
@@ -361,7 +346,7 @@ if __name__ == "__main__":
     FLAGS = parser.parse_args()
 
     # clear logger.
-    logging.basicConfig(level=logging.DEBUG, filename="script_status.log")
+    logging.basicConfig(level=logging.DEBUG, filename="script_status_subtask3.log")
 
     logger = logging.getLogger("IML-P2-T1T2")
 
