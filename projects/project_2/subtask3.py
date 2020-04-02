2daf11cff7946c5bd8871dffabe3e365fa9bbe51
Philip Hartout <philip.hartout@protonmail.com>
2020-04-02 14:36:27 +0200

    :twisted_rightwards_arrows: add neural network

--- projects/project_2/subtask3.py
+++ projects/project_2/subtask3.py
@@ -29,14 +29,19 @@ __email__ = "jyates@student.ethz.ch; phartout@student.ethz.ch"
 import multiprocessing
 import argparse
 import logging
+import sys
+import torch
 
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import ADASYN, SMOTE
 from imblearn.under_sampling import ClusterCentroids, RandomUnderSampler
 from sklearn.svm import SVR
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 TYPICAL_VALUES = {
     "pid": 15788.831218741774,
@@ -184,10 +189,18 @@ def data_preprocessing(df_train, df_train_label, df_test, logger):
 
     X_train = scaler.fit_transform(X_train)
 
-    return X_train, y_train_vital_signs, df_test_preprocessed
+    # Also transform the outputs as it may be better for the network
+    vital_signs_scales = []
+    # if FLAGS.model == "ANN":
+    #
+    #     for i, sign in enumerate(VITAL_SIGNS):
+    #         y_train_vital_signs[i] = scaler.fit_transform(y_train_vital_signs[i].reshape(-1, 1))
+    #         vital_signs_scales.append(scaler.scale_)
+    #         y_train_vital_signs[i] = y_train_vital_signs[i].reshape(-1)
+    return X_train, y_train_vital_signs, df_test_preprocessed, vital_signs_scales
 
 
-def get_vital_signs_models(X_train, y_train_vital_signs, param_grid):
+def get_vital_signs_svr_models(X_train, y_train_vital_signs, param_grid):
     svr_models = []
     scores = []
     for i, test in enumerate(VITAL_SIGNS):
@@ -209,7 +222,7 @@ def get_vital_signs_models(X_train, y_train_vital_signs, param_grid):
     return svr_models, scores
 
 
-def get_vital_signs_predictions(X_test, test_pids, models):
+def get_vital_signs_predictions(X_test, test_pids, models, vital_signs_scales, device):
     """Function to obtain predictions for every model, as a confidence level : the closer to 1
     (resp 0), the more confidently) the sample belongs to class 1 (resp 0).
 
@@ -225,14 +238,105 @@ def get_vital_signs_predictions(X_test, test_pids, models):
     """
     df_pred = pd.DataFrame()
 
-    for i, test in enumerate(VITAL_SIGNS):
-        # Compute prediction
-        y_pred = models[i - 1].predict(X_test)
 
-        y_mean = [np.mean(y_pred[i : i + 12]) for i in range(len(test_pids))]
-        df = pd.DataFrame({test: y_mean}, index=test_pids)
-        df_pred = pd.concat([df_pred, df], axis=1)
+    for i, test in enumerate(VITAL_SIGNS):
+        if FLAGS.model == "SVR":
+            # Compute prediction
+            y_pred = models[i].predict(X_test)
+            y_mean = [np.mean(y_pred[i : i + 12]) for i in range(len(test_pids))]
+            df = pd.DataFrame({test: y_mean}, index=test_pids)
+            df_pred = pd.concat([df_pred, df], axis=1)
+        else:
+            # Switch network to eval mode to make sure all dropout layers are there, etc..
+            y_pred = models[i](torch.from_numpy(X_test).to(device).float()).cpu().detach().numpy()
+            y_mean = [np.mean(y_pred[i: i + 12]) for i in range(len(test_pids))]
+            df = pd.DataFrame({test: y_mean}, index=test_pids)
+            df_pred = pd.concat([df_pred, df], axis=1)
     return df_pred
+
+def convert_to_cuda_tensor(X_train, X_test, y_train, y_test, device):
+    return torch.from_numpy(X_train).to(device).float(), torch.from_numpy(X_test).to(device).float(), torch.from_numpy(y_train).to(device).float(), \
+           torch.from_numpy(y_test).to(device).float()
+
+### Define network
+class Feedforward(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(Feedforward, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(self.hidden_size, hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.fc3 = torch.nn.Linear(self.hidden_size, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        hidden = self.fc1(x)
+        relu = self.relu(hidden)
+        hidden_2 = self.fc2(relu)
+        relu_2 = self.relu(hidden_2)
+        output = self.fc3(relu_2)
+        output = self.sigmoid(output)
+        return output
+
+
+class Data(Dataset):
+    def __init__(self,x ,y):
+        self.x = x
+        self.y = y
+        self.len = self.x.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.len
+
+
+def get_vital_signs_ann_models(x_input, y_input, logger, device):
+    logger.info(f"Using {device} to train the neural network.")
+    ann_models = []
+    scores = []
+    for i, sign in enumerate(VITAL_SIGNS):
+        logger.info(f"Starting neural network training for vital sign {sign}")
+        X_train, X_test, y_train, y_test = train_test_split(x_input, y_input[i], test_size=0.10,
+                                                            random_state=42,
+                                                            shuffle=True)
+        logger.info("Converting arrays to tensors")
+        X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor = convert_to_cuda_tensor(X_train, X_test, y_train, y_test, device)
+        model = Feedforward(35, 80)
+        criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+        dataset = Data(X_train_tensor, y_train_tensor)
+        batch_size = 64  # Ideally we want any multiple of 12 here
+        trainloader = DataLoader(dataset=dataset, batch_size=batch_size)
+        LOSS = []
+        if torch.cuda.is_available():
+            model.cuda()
+        model.float()
+        writer = SummaryWriter()
+        X_test_tensor = X_test_tensor.to(device)
+        for epoch in range(FLAGS.epochs):
+            for x, y in trainloader:
+                yhat = model(x)
+                loss = criterion(yhat.float(), y.reshape((y.shape[0], 1)))
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                LOSS.append(loss)
+                y_pred = model(X_test_tensor).cpu().detach().numpy()
+                test_loss = mean_squared_error(y_test, y_pred)
+                writer.add_scalar('Loss/train', loss, epoch)
+                writer.add_scalar('Loss/test', test_loss, epoch)
+
+        writer.close()
+        test_error = mean_squared_error(y_test, y_pred)
+        logger.info(f"MSE for test set is {test_error}")
+        logger.info(f"Finished test for vital sign {sign}")
+        ann_models.append(model)
+        scores.append(test_error)
+    return ann_models
 
 
 def main(logger):
@@ -249,36 +353,42 @@ def main(logger):
     df_train, df_train_label, df_test = load_data()
     logger.info("Finished Loading data")
 
-    X_train, y_train_vital_signs, df_test_preprocessed = data_preprocessing(
+    X_train, y_train_vital_signs, df_test_preprocessed, vital_signs_scales = data_preprocessing(
         df_train, df_train_label, df_test, logger
     )
 
     logger.info("Beginning modelling process.")
 
-    # train model here
-    param_grid = {
-        "kernel": ["linear", "poly", "rbf", "sigmoid"],
-        "degree": np.arange(1, 4, 1),
-        "gamma": np.linspace(0.1, 10, num=3),
-        "coef0": [0],
-        "tol": [0.001],
-        "C": np.linspace(0.1, 10, num=3),
-        "epsilon": [0.1],
-        "shrinking": [True],
-        "cache_size": [1000],
-        "verbose": [False],
-        "max_iter": [1000],
-    }
-    vital_signs_models, vital_signs_models_scores = get_vital_signs_models(
-        X_train, y_train_vital_signs, param_grid
-    )
+    if FLAGS.model == "SVR":
+        # train model here
+        param_grid = {
+            "kernel": ["linear", "poly", "rbf", "sigmoid"],
+            "degree": np.arange(1, 4, 1),
+            "gamma": np.linspace(0.1, 10, num=3),
+            "coef0": [0],
+            "tol": [0.001],
+            "C": np.linspace(0.1, 10, num=3),
+            "epsilon": [0.1],
+            "shrinking": [True],
+            "cache_size": [1000],
+            "verbose": [False],
+            "max_iter": [1000],
+        }
+        vital_signs_models, vital_signs_models_scores = get_vital_signs_svr_models(
+            X_train, y_train_vital_signs, param_grid
+        )
+    else:
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        vital_signs_models = get_vital_signs_ann_models(
+            X_train, y_train_vital_signs, logger, device
+        )
 
     # get the unique test ids of patients
     test_pids = np.unique(df_test_preprocessed[IDENTIFIERS].values)
     logger.info("Fetch predictions.")
     X_test = df_test_preprocessed.drop(columns=IDENTIFIERS).values
     vital_signs_predictions = get_vital_signs_predictions(
-        X_test, test_pids, vital_signs_models
+        X_test, test_pids, vital_signs_models, vital_signs_scales, device
     )
 
     logger.info("Export predictions DataFrame to a zip file")
@@ -351,8 +461,26 @@ if __name__ == "__main__":
         "--k_fold",
         "-k",
         type=int,
-        required=True,
+        required=False,
         help="k to perform k-fold cv in the gridsearch",
+    )
+
+    parser.add_argument(
+        "--model",
+        "-m",
+        type=str,
+        required=True,
+        help="whether to train a neural network or an SVR for the tasks",
+        choices=["SVR", "ANN"],
+    )
+
+    parser.add_argument(
+        "--epochs",
+        "-ep",
+        type=int,
+        required=False,
+        help="",
+        default=100
     )
 
     FLAGS = parser.parse_args()
@@ -362,7 +490,7 @@ if __name__ == "__main__":
         level=logging.DEBUG, filename="script_status_subtask3.log"
     )
 
-    logger = logging.getLogger("IML-P2-T1T2")
+    logger = logging.getLogger("IML-P2-T3")
 
     # Create a second stream handler for logging to `stderr`, but set
     # its log level to be a little bit smaller such that we only have
@@ -378,5 +506,10 @@ if __name__ == "__main__":
         )
     )
     logger.addHandler(stream_handler)
+
+    logger.info("Usage:\n{0}\n".format(" ".join([x for x in sys.argv])))
+    logger.info("All settings used:")
+    for k, v in sorted(vars(FLAGS).items()):
+        logger.info("{0}: {1}".format(k, v))
 
     main(logger)
