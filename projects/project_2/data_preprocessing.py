397158662a362391146c6aa290757e8acddf72b6
Philip Hartout <philip.hartout@protonmail.com>
2020-04-08 12:11:31 +0200

    :wrench: add modifications to preprocessing pipeline

--- projects/project_2/data_preprocessing.py
+++ projects/project_2/data_preprocessing.py
@@ -4,12 +4,14 @@
 Script to preprocess the dataset and export it for other modelling scripts
 
 example usage from CLI:
- $ python3 data_preprocessing.py --train_features path/to/data/train_features.csv
-                    --train_labels path/to/data/train_labels.csv
-                    --test_features path/to/data/test_features.csv
-                    --preprocess_train path/to/data/preprocessed_train.csv
-                    --preprocess_test path/to/data/preprocessed_test.csv
-                    --preprocess_label path/to/data/preprocessed_labels.csv
+ $ python3 data_preprocessing.py --train_features TRAIN_FEATURES
+                             --test_features TEST_FEATURES
+                             --train_labels TRAIN_LABELS
+                             --preprocess_train PREPROCESS_TRAIN
+                             --preprocess_test PREPROCESS_TEST
+                             --preprocess_train_label PREPROCESS_TRAIN_LABEL
+                             [--nb_of_patients NB_OF_PATIENTS]
+
 For help, run:
  $ python data_preprocessing.py -h
 
@@ -22,7 +24,12 @@ Following Google style guide: http://google.github.io/styleguide/pyguide.html
 import argparse
 import logging
 
+import seaborn as sns
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from tqdm import tqdm
 
 TYPICAL_VALUES = {
     "pid": 15788.831218741774,
@@ -90,7 +97,7 @@ def load_data():
 
     """
     if FLAGS.nb_of_patients is not None:
-        rows_to_load = (FLAGS.nb_of_patients * 12) + 1
+        rows_to_load = (FLAGS.nb_of_patients * 12)
     else:
         rows_to_load = None
     df_train = pd.read_csv(FLAGS.train_features, nrows=rows_to_load)
@@ -112,10 +119,7 @@ def fill_na_with_average_patient_column(df, logger):
         df (pandas.core.frame.DataFrame): dataframe containing the transformed data
     """
     columns = list(df.columns)
-    for i, column in enumerate(columns):
-        logger.info(
-            "{} column of {} columns processed".format(i + 1, len(columns))
-        )
+    for i, column in tqdm(enumerate(columns)):
         # Fill na with patient average
         df[[column]] = df.groupby(["pid"])[column].transform(
             lambda x: x.fillna(x.mean())
@@ -128,6 +132,96 @@ def fill_na_with_average_patient_column(df, logger):
         for column in columns_with_na:
             df[column] = TYPICAL_VALUES[column]
     return df
+
+
+def missing_datadata_imputer(df_train, logger):
+    """Imputes data and returns a dataframe with one row per patient filled with the values as
+    follows:
+        * If the amount of data that is there is above 8, then fit a linear regression and
+            take the coefficient as input
+        * If the amount of data that is there is below 8, take the average
+        * If no data is present fill the data with the column average or a typical value if no
+            values is present in the case that a restricted number of patients are loaded.
+
+
+    Args:
+        df_train (pandas.core.Dataframe): input dataframe with the features that need to be
+            augmented.
+        logger (Logger): logger.
+
+    Returns:
+        pandas.core.DataFrame
+
+    """
+
+    logger.info("Creating missing data dataframe")
+    pid = df_train["pid"].unique()
+    df_na = pd.DataFrame(columns=df_train.columns)
+    processed_df_columns = [x for x in df_train.columns if x not in IDENTIFIERS]
+    df_train_preprocessed = pd.DataFrame(columns=processed_df_columns, index=pid)
+    for patient in tqdm(pid):
+        df_na = df_na.append(df_train.loc[df_train['pid'] == patient].isna().sum(),
+                             ignore_index=True)
+
+    logger.info("Getting quantile information")
+    column_quantiles = {}
+    columns = df_na.columns.tolist()
+    columns = [x for x in columns if x not in IDENTIFIERS]
+    for column in tqdm(columns):
+        column_quantiles[column] = df_na[column].quantile(.25)
+
+    columns_for_regression = [k for k, v in column_quantiles.items() if float(v) <= 8]
+    columns_for_regression.remove("Age")
+    columns_for_averaging = np.setdiff1d(np.array(df_train.columns),
+                                         np.array(columns_for_regression)).tolist()
+    columns_for_regression_formatted = [sub + "_trend" for sub in columns_for_regression]
+    columns_for_averaging = [x for x in columns_for_averaging if x not in IDENTIFIERS]
+    df_train_preprocessed = df_train_preprocessed.reindex(df_train_preprocessed.columns.tolist() +
+                                                          columns_for_regression_formatted, axis=1)
+
+    logger.info("Commencing data imputation process")
+    logger.info(f"Add trends for: {columns_for_regression}")
+    for patient in tqdm(pid):
+        for column in columns_for_regression:
+            if df_train.loc[df_train['pid'] == patient][column].isna().sum() <= 8:
+                series = df_train.loc[df_train['pid'] == patient][column]
+                # Fill missing values between two non nans with their average
+                series = (series.ffill() + series.bfill
+                ()) / 2
+                # Drop the rest of the value
+                series = series.dropna()
+                X = [i for i in range(0, len(series))]
+                X = np.reshape(X, (len(X), 1))
+                y = series
+                model = LinearRegression()
+                model.fit(X, y)
+                df_train_preprocessed.at[patient, column + "_trend"] = model.coef_
+
+    # fill rest of values with 0 for trends col umns
+    df_train_preprocessed[columns_for_regression_formatted] = df_train_preprocessed[columns_for_regression_formatted]\
+        .fillna(value=0)
+
+    # Where it is not reasonable to fit a line, we can still take the average of all available
+    # values and use that as feature
+    logger.info("Imputing values for columns where there is not enough information for trends"
+                "but enough to make an average for each patient.")
+    for patient in tqdm(pid):
+        for column in columns_for_averaging:
+            df_train_preprocessed.at[patient, column] = df_train.loc[df_train['pid'] ==
+                                                                     patient][column].mean()
+
+    logger.info("Imputing for the remaining columns where no specific patient average can be found")
+    # Fill na with overall column average for lack of a better option for now
+    df_train_preprocessed = df_train_preprocessed.fillna(df_train_preprocessed.mean())
+    # This is only when testing where there is not always data for all loaded rows.
+    if df_train_preprocessed.isnull().values.any():
+        columns_with_na = df_train_preprocessed.columns[df_train_preprocessed.isna().any()].tolist()
+        for column in columns_with_na:
+            df_train_preprocessed[column] = TYPICAL_VALUES[column]
+
+    df_train_preprocessed = df_train_preprocessed.reset_index()
+    df_train_preprocessed = df_train_preprocessed.rename(columns={"index": "pid"})
+    return df_train_preprocessed
 
 
 def main(logger):
@@ -144,9 +238,13 @@ def main(logger):
     df_train, df_train_label, df_test = load_data()
     logger.info("Finished Loading data")
 
-    logger.info("Preprocess training set")
+    logger.info("Preprocessing training set")
     # Would be useful to distribute/multithread this part
-    df_train_preprocessed = fill_na_with_average_patient_column(
+    # df_train_preprocessed = fill_na_with_average_patient_column(
+    #     df_train, logger
+    # )
+
+    df_train_preprocessed = missing_datadata_imputer(
         df_train, logger
     )
 
@@ -154,7 +252,8 @@ def main(logger):
     df_train_label[MEDICAL_TESTS + VITAL_SIGNS + SEPSIS] = df_train_label[
         MEDICAL_TESTS + VITAL_SIGNS + SEPSIS
     ].astype(int)
-    # Merging pids to make sure they map correctly.
+
+    # Merging pids to make sure they map correctly and we did not mess up our preprocessing.
     df_train_preprocessed_merged = pd.merge(
         df_train_preprocessed,
         df_train_label,
@@ -163,14 +262,14 @@ def main(logger):
         right_on="pid",
     )
 
-    df_train_label_preprocessed = df_train_preprocessed_merged[IDENTIFIERS + MEDICAL_TESTS + VITAL_SIGNS+ SEPSIS]
-    logger.info("Preprocess test set")
-    df_test_preprocessed = fill_na_with_average_patient_column(df_test, logger)
+    df_train_label_preprocessed = df_train_preprocessed_merged[["pid"] + MEDICAL_TESTS + VITAL_SIGNS+ SEPSIS]
+    logger.info("Preprocessing test set")
+    df_test_preprocessed = missing_datadata_imputer(df_test, logger)
 
     logger.info("Export preprocessed train and test set")
     # Export pandas dataframe to csv.
     df_train_label_preprocessed.to_csv(
-        FLAGS.preprocess_label, index=False, float_format="%.3f"
+        FLAGS.preprocess_train_label, index=False, float_format="%.3f"
     )
     df_train_preprocessed.to_csv(
         FLAGS.preprocess_train, index=False, float_format="%.3f"
@@ -232,12 +331,20 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--preprocess_label",
+        "--preprocess_train_label",
         "-pre_label",
         type=str,
         required=True,
         help="path to the CSV file containing the training \
                                 label data",
+    )
+
+    parser.add_argument(
+        "--nb_of_patients",
+        "-b_path",
+        type=int,
+        required=False,
+        help="number of patients to load",
     )
 
     FLAGS = parser.parse_args()
