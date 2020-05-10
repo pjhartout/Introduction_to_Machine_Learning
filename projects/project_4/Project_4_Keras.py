f1a12c6c5dbc8283140b5fbee72a4719ad0971fa
Philip Hartout <philip.hartout@protonmail.com>
2020-05-10 16:53:40 +0200

    :pencil: add source (script training on GCP)

--- projects/project_4/Project_4_Keras.py
+++ projects/project_4/Project_4_Keras.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# Implementation of triplet loss using keras inspired by https://github.com/noelcodella/tripletloss-keras-tensorflow
 
-
-# !pip install tensorflow-gpu
-
-
-# In[1]:
+# In[2]:
 
 
 import keras
+import tensorflow as tf
+import tensorflow_addons as tfa
 
 
-# In[2]:
+# In[3]:
 
 
 import keras.applications
@@ -25,7 +23,7 @@ from keras.preprocessing.image import img_to_array
 from keras import optimizers
 
 
-# In[3]:
+# In[4]:
 
 
 import sys
@@ -33,10 +31,10 @@ import os
 import numpy as np
 import pandas as pd
 import cv2 
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 
-# In[4]:
+# In[5]:
 
 
 print(keras.__version__)
@@ -46,51 +44,17 @@ from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
 
-# In[5]:
-
-
-T_G_WIDTH = 300
-T_G_HEIGHT = 400
-T_G_NUMCHANNELS = 3
-CHUNKSIZE = 16
-BATCHSIZE = 4
-
-
 # In[6]:
 
 
-"""from keras.preprocessing.image import ImageDataGenerator
-
-# Generator object for data augmentation.
-# Can change values here to affect augmentation style.
-datagen = ImageDataGenerator(  rotation_range=90,
-                                width_shift_range=0.05,
-                                height_shift_range=0.05,
-                                zoom_range=0.1,
-                                horizontal_flip=True,
-                                vertical_flip=True,
-                                )"""
+T_G_WIDTH = 150
+T_G_HEIGHT = 150
+T_G_NUMCHANNELS = 3
+CHUNKSIZE = 32
+BATCHSIZE = 2048
 
 
 # In[7]:
-
-
-# generator function for data augmentation
-"""def createDataGen(X1, X2, X3, Y, b):
-
-    local_seed = 44
-    genX1 = datagen.flow(X1,Y, batch_size=b, seed=local_seed, shuffle=False)
-    genX2 = datagen.flow(X2,Y, batch_size=b, seed=local_seed, shuffle=False)
-    genX3 = datagen.flow(X3,Y, batch_size=b, seed=local_seed, shuffle=False)
-    while True:
-        X1i = genX1.next()
-        X2i = genX2.next()
-        X3i = genX3.next()
-
-        yield [X1i[0], X2i[0], X3i[0]], X1i[1]"""
-
-
-# In[8]:
 
 
 train_triplets = pd.read_csv("data/train_triplets.txt", names=["A", "B", "C"], sep=" ")
@@ -104,7 +68,7 @@ for column in ["A", "B", "C"]:
 train_triplets.head()
 
 
-# In[9]:
+# In[8]:
 
 
 # split in test and training set, we take 0.3 of the dataframe and use it for testing and the rest for training
@@ -114,31 +78,85 @@ test_images = train_triplets[:n_test]
 train_images = train_triplets[n_test:]
 
 
+# In[9]:
+
+
+# def createCNNModel(emb_size):
+
+#     # Initialize a ResNet50_ImageNet Model
+# #     resnet_input = kl.Input(shape=(T_G_WIDTH,T_G_HEIGHT,T_G_NUMCHANNELS))
+#     model = tf.keras.Sequential([
+#         tf.keras.layers.Conv2D(filters=10, kernel_size=2, padding='same', activation='relu', input_shape=(T_G_HEIGHT,T_G_WIDTH,3)),
+#         tf.keras.layers.MaxPooling2D(pool_size=2),
+#         tf.keras.layers.Dropout(0.3),
+#         tf.keras.layers.Flatten(),
+#         tf.keras.layers.Dense(emb_size, activation=None), # No activation on final dense layer
+#         tf.keras.layers.Lambda(lambda x: tf.math.l2_normalize(x, axis=1)) # L2 normalize embeddings
+#     ])
+
+#     print(type(model))
+#     # triplet framework, shared weights
+#     input_shape=(T_G_WIDTH,T_G_HEIGHT,T_G_NUMCHANNELS)
+#     input_anchor = kl.Input(shape=input_shape, name='input_anchor')
+#     input_positive = kl.Input(shape=input_shape, name='input_pos')
+#     input_negative = kl.Input(shape=input_shape, name='input_neg')
+#     print(type(model))
+#     print(type(input_positive))
+#     model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
+#     loss=tfa.losses.TripletSemiHardLoss())
+#     net_anchor = model(input_anchor)
+#     net_positive = model(input_positive)
+#     net_negative = model(input_negative)
+
+#     # The Lamda layer produces output using given function. Here its Euclidean distance.
+#     positive_dist = kl.Lambda(euclidean_distance, name='pos_dist')([net_anchor, net_positive])
+#     negative_dist = kl.Lambda(euclidean_distance, name='neg_dist')([net_anchor, net_negative])
+#     tertiary_dist = kl.Lambda(euclidean_distance, name='ter_dist')([net_positive, net_negative])
+
+#     # This lambda layer simply stacks outputs so both distances are available to the objective
+#     stacked_dists = kl.Lambda(lambda vects: K.stack(vects, axis=1), name='stacked_dists')([positive_dist, negative_dist, tertiary_dist])
+
+#     model = Model([input_anchor, input_positive, input_negative], stacked_dists, name='triple_siamese')
+
+#     v_optimizer = optimizers.Adam(lr=0.001)
+
+#     model.compile(optimizer=v_optimizer, loss=triplet_loss, metrics=[accuracy])
+
+#     return model
+# createCNNModel(100)
+
+
 # In[10]:
 
 
-def createModel(emb_size):
+def createResNetModel(emb_size):
 
     # Initialize a ResNet50_ImageNet Model
-    resnet_input = kl.Input(shape=(T_G_WIDTH,T_G_HEIGHT,T_G_NUMCHANNELS))
-    resnet_model = keras.applications.resnet50.ResNet50(weights=None, include_top = False, input_tensor=resnet_input)
+    xception_input = kl.Input(shape=(T_G_WIDTH,T_G_HEIGHT,T_G_NUMCHANNELS))
+    xception_model = keras.applications.Xception(include_top=False,
+        weights=None,
+        input_tensor=xception_input,
+        input_shape=None,
+        pooling=max,
+    )
 
     # New Layers over ResNet50
-    net = resnet_model.output
+    net = xception_model.output
     net = kl.GlobalAveragePooling2D(name='gap')(net)
     net = kl.Dropout(0.5)(net)
     net = kl.Dense(emb_size,activation='relu',name='t_emb_1')(net)
     net = kl.Lambda(lambda  x: K.l2_normalize(x,axis=1), name='t_emb_1_l2norm')(net)
-
+    
     # model creation
-    base_model = Model(resnet_model.input, net, name="base_model")
+    base_model = Model(xception_model.input, net, name="base_model")
 
     # triplet framework, shared weights
     input_shape=(T_G_WIDTH,T_G_HEIGHT,T_G_NUMCHANNELS)
     input_anchor = kl.Input(shape=input_shape, name='input_anchor')
     input_positive = kl.Input(shape=input_shape, name='input_pos')
     input_negative = kl.Input(shape=input_shape, name='input_neg')
-
+    print(type(base_model))
+    print(type(input_positive))
     net_anchor = base_model(input_anchor)
     net_positive = base_model(input_positive)
     net_negative = base_model(input_negative)
@@ -195,7 +213,7 @@ def t_read_image(loc):
 
 # load file names
 for direc, subdir, file in os.walk(r"data/food"):
-    list_dir = file[1:]
+    list_dir = file[:]
 
 
 # In[14]:
@@ -204,6 +222,7 @@ for direc, subdir, file in os.walk(r"data/food"):
 # load the image
 img_array = {}
 for file in tqdm(list_dir):
+#     print(file)
     img = t_read_image(os.path.join("data/food", file))
     img_array[file.split(".jpg")[0]]=img_to_array(img)
 
@@ -211,15 +230,32 @@ for file in tqdm(list_dir):
 # In[15]:
 
 
-img_array
+# create model
+# int is the embedding size 
+# resnet_model = createResNetModel(300)
 
 
 # In[16]:
 
 
-# create model
-# int is the embedding size 
-model = createModel(300)
+use_pretrained_model = False
+if use_pretrained_model == False:
+    cnn_model = createResNetModel(300) 
+else:
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    cnn_model = keras.models.model_from_json(loaded_model_json)
+    # load weights into new model
+    cnn_model.load_weights("Xception.h5")
+    cnn_model.compile(optimizer=optimizers.Adam(lr=0.001), loss=triplet_loss, metrics=[accuracy])
+    print("Loaded model from disk")
+
+
+# In[ ]:
+
+
+
 
 
 # In[17]:
@@ -244,63 +280,119 @@ print("Getting negatives test ...")
 negatives_test = [img_array[img] for img in np.array(test_images["C"])]
 
 
-# In[19]:
+# In[22]:
 
 
-numepochs = 1
+numepochs = 100
 total_t_ch = int(np.ceil(len(anchors_train) / float(CHUNKSIZE)))
 total_v_ch = int(np.ceil(len(anchors_test) / float(CHUNKSIZE)))
 
 
-# In[20]:
+# In[ ]:
 
 
-for e in range(0, numepochs):
+for e in tqdm(range(0, numepochs)):
+    for t in tqdm(range(0, total_t_ch)):
+        print ("Epoch :{}, train chunk {}/{}".format(e,t+1,total_t_ch))
+        anchors_t = anchors_train[t*CHUNKSIZE:(t+1)*CHUNKSIZE]
+        positives_t =positives_train[t*CHUNKSIZE:(t+1)*CHUNKSIZE]
+        negatives_t = negatives_train[t*CHUNKSIZE:(t+1)*CHUNKSIZE]
+        Y_train = np.random.randint(2, size=(1,2,len(anchors_t))).T
+        # This method does NOT use data augmentation
+        cnn_model.fit([anchors_t, positives_t, negatives_t], Y_train, epochs=1,  batch_size=BATCHSIZE)        
 
-        for t in range(0, total_t_ch):
 
-            print ("Epoch :{}, train chunk {}/{}...".format(e,t+1,total_t_ch))
+# In[ ]:
 
-            print('Reading image lists ...')
-            anchors_t = anchors_train[t*CHUNKSIZE:(t+1)*CHUNKSIZE]
-            positives_t =positives_train[t*CHUNKSIZE:(t+1)*CHUNKSIZE]
-            negatives_t = negatives_train[t*CHUNKSIZE:(t+1)*CHUNKSIZE]
-            Y_train = np.random.randint(2, size=(1,2,len(anchors_t))).T
 
-            print('Starting to fit ...')
-            # This method does NOT use data augmentation
-            model.fit([anchors_t, positives_t, negatives_t], Y_train, epochs=numepochs,  batch_size=BATCHSIZE)
+# In case the validation images don't fit in memory, we load chunks from disk again. 
+val_res = [0.0, 0.0]
+total_w = 0.0
+for v in range(0, total_v_ch):
 
-            # This method uses data augmentation
-            # model.fit_generator(generator=createDataGen(anchors_t,positives_t,negatives_t,Y_train,BATCHSIZE), 
-             #                   steps_per_epoch=len(Y_train) / BATCHSIZE, epochs=1, 
-            #                  shuffle=False, use_multiprocessing=True)
-        
+    print('Loading validation image lists ...')
+    print ("Epoch :{}, train chunk {}/{}...".format(e,v+1,total_v_ch))
+    anchors_v = anchors_test[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
+    positives_v =positives_test[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
+    negatives_v = negatives_test[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
+    Y_val = np.random.randint(2, size=(1,2,len(anchors_v))).T
 
-        # In case the validation images don't fit in memory, we load chunks from disk again. 
-        val_res = [0.0, 0.0]
-        total_w = 0.0
-        for v in range(0, total_v_ch):
+    # Weight of current validation measurement. 
+    # if loaded expected number of items, this will be 1.0, otherwise < 1.0, and > 0.0.
+    w = float(len(anchors_v)) / float(CHUNKSIZE)
+    total_w = total_w + w
 
-            print('Loading validation image lists ...')
-            print ("Epoch :{}, train chunk {}/{}...".format(e,v+1,total_v_ch))
-            anchors_v = anchors_test[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
-            positives_v =positives_test[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
-            negatives_v = negatives_test[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
-            Y_val = np.random.randint(2, size=(1,2,len(anchors_v))).T
+    curval = cnn_model.evaluate([anchors_v, positives_v, negatives_v], Y_val, batch_size=BATCHSIZE)
+    val_res[0] = val_res[0] + w*curval[0]
+    val_res[1] = val_res[1] + w*curval[1]
+#     print(pd.Series(np.argmax(val_res)).value_counts())
+val_res = [x / total_w for x in val_res]
 
-            # Weight of current validation measurement. 
-            # if loaded expected number of items, this will be 1.0, otherwise < 1.0, and > 0.0.
-            w = float(anchors_v.shape[0]) / float(CHUNKSIZE)
-            total_w = total_w + w
+print('Validation Results: ', str(val_res))
 
-            curval = model.evaluate([anchors_v, positives_v, negatives_v], Y_val, batch_size=BATCHSIZE)
-            val_res[0] = val_res[0] + w*curval[0]
-            val_res[1] = val_res[1] + w*curval[1]
 
-        val_res = [x / total_w for x in val_res]
+# In[ ]:
 
-        print('Validation Results: ', str(val_res))
+
+# serialize model to JSON
+model_json = cnn_model.to_json()
+with open("model_2.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+cnn_model.save_weights("Xception_2.h5")
+print("Saved model to disk")
+ 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# Now we want to generate the output 0, for each triplet of image on the validation to get the score
+
+
+# In[ ]:
+
+
+print("Getting anchors test ...")
+anchors_val = [img_array[img] for img in np.array(test_triplets["A"])]
+print("Getting first images ...")
+first_val = [img_array[img] for img in np.array(test_triplets["B"])]
+print("Getting second test ...")
+second_val = [img_array[img] for img in np.array(test_triplets["C"])]
+
+
+# In[ ]:
+
+
+total_v_ch = int(np.ceil(len(anchors_val) / float(CHUNKSIZE)))
+# for each chunk we have to compute the embedding and the distance to the closest neighbour.
+predictions_list = []
+errors = 0
+for v in tqdm(range(0, total_v_ch)):
+    anchors_val_chunk = anchors_val[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
+    first_val_chunk =first_val[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
+    second_val_chunk = second_val[v*CHUNKSIZE:(v+1)*CHUNKSIZE]
+    predictions = cnn_model.predict([anchors_val_chunk, first_val_chunk, second_val_chunk], batch_size=BATCHSIZE)
+    for distance in predictions:
+        predictions_list.append(np.argmin(np.array([distance[1], distance[0]])))
+
+
+# In[ ]:
+
+
+predictions_array = np.asarray(predictions_list)
+
+
+# In[ ]:
+
+
+np.savetxt('predictions.txt', predictions_array, fmt='%d', delimiter='\n')
 
 
 # In[ ]:
